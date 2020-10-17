5681cac655095eae861ca8a5c08fcebfb8ec916c
BreJohn <i.bregiannis@gmail.com>
2020-10-17 21:15:28 +0300

    rmv comments

--- dashboard/src/app/components/map/map.component.ts
+++ dashboard/src/app/components/map/map.component.ts
@@ -11,12 +11,8 @@ export class MapComponent {
   @ViewChild('mapContainer', { static: false }) gmap: ElementRef;
   hidden = false;
   btnLabel = 'Hide';
-  // map: google.maps.Map;
-  // marker: google.maps.Marker;
   lat: number;
   lng: number;
-  // coordinates: google.maps.LatLng;
-  // mapOptions: google.maps.MapOptions;
 
   ngOnInit() {
     this.initMap();
@@ -27,29 +23,13 @@ export class MapComponent {
       navigator.geolocation.getCurrentPosition((pos) => {
         this.lng = +pos.coords.longitude;
         this.lat = +pos.coords.latitude;
-        // this.coordinates = new google.maps.LatLng(this.lat, this.lng);
-        // this.mapOptions = {
-        //   center: this.coordinates,
-        //   zoom: 8,
-        // };
-        // this.createMap();
       });
     }
   }
 
-  // createMap() {
-  //   this.map = new google.maps.Map(this.gmap.nativeElement, this.mapOptions);
-  //   this.marker = new google.maps.Marker({
-  //     position: this.coordinates,
-  //     map: this.map,
-  //   });
-  // }
   toggleCal(): void {
     const calendarState = toggleCalendar(this.hidden);
     this.btnLabel = calendarState.btnLabel;
     this.hidden = calendarState.hidden;
-    // if (!this.hidden) {
-    //   this.initMap();
-    // }
   }
 }
